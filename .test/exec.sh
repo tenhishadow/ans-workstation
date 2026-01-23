2ac17d509c09ecb2cbae4a4a997f8aabe79249e3
tenhishadow <adm@tenhi.dev>
2026-01-23 22:45:44 +0100

    fix

--- .test/exec.sh
+++ .test/exec.sh
@@ -6,7 +6,8 @@ export UV_PYPY_INSTALL_MIRROR="https://nexus.tenhi.dev/repository/pypi/simple"
 
 pacman -Sy --noconfirm go-task git
 
+# go-task -- --skip-tags pkg
 go-task
 
 # idempotency
-go-task
+# go-task -- --skip-tags pkg
