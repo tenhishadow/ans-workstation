6fa3b6eff45a04670ec5f5c4e6e7722fb21f1358
tenhishadow <adm@tenhi.dev>
2026-03-05 23:27:01 +0100

    fix

--- .test/exec.sh
+++ .test/exec.sh
@@ -7,6 +7,18 @@ export ANSIBLE_FORCE_COLOR='true'
 
 pacman --disable-sandbox -Sy --noconfirm --needed --noprogressbar reflector go-task git sudo >/dev/null
 
+sudo reflector \
+  --ipv4 \
+  --protocol https \
+  --completion-percent 95 \
+  --score 10 \
+  --latest 30 \
+  --fastest 10 \
+  --threads 8 \
+  --connection-timeout 1 \
+  --download-timeout 2 \
+  --save /etc/pacman.d/mirrorlist
+
 go-task -- --skip-tags pkg
 
 # idempotency
